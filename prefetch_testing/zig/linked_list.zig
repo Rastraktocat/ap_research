bd6fee06e3ea58bf3dabd0c095a926297d54c9f5
Adi <adindeshpande@gmail.com>
2026-09-06 13:46:53 -0500

    added the prefetch folder

---
+++ prefetch_testing/zig/linked_list.zig
@@ -0,0 +1,84 @@
+const std = @import("std");
+
+var Node = struct {
+
+	value: type,
+	next: ?*@This(),
+
+};
+
+pub fn linked_list(T: type, allocator: std.mem.Allocator) Node {
+
+	return struct {
+		value: T,
+		next: ?*Node,
+
+		fn get_next() Node {
+
+			const node = *@This();
+			return node.next.*;
+
+		} // get_next() 
+
+		fn set_next(ptr: ?*@This()) void {
+
+			const node = *@This();
+			node.next.*.next = ptr;
+
+		} // set_next()
+
+		fn set_value(value_set: T) void {
+
+			const node = *@This();
+			node.next.*.value = value_set;
+
+		} // set_value()
+
+		fn del_node(prev_node: *@This()) void {
+
+			var node = *@This();
+			prev_node.set_next(node.get_next());
+			try allocator.free(node);
+
+		} // del_node()
+
+		fn add_node(prev_node: *@This(), add_node_value: T) void {
+			
+			var node = *@This();
+			var addition_node: T = try allocator.alloc(node, @sizeOf(node));
+			
+			prev_node.set_next(*node);
+
+			addition_node.set_value(add_node_value);
+			addition_node.set_next(node.get_next());
+
+			Node.set_next(add_node.next);
+
+		} // add_node()
+
+	}; // struct Node
+
+}
+
+pub fn make_idx_list(comptime T: type) type {
+
+	return struct {
+		value: T,
+		next: ?@This(), 
+		idx: i32,
+
+	};
+
+}
+
+pub fn main() !void {
+
+	var gpa = std.heap.DebugAllocator(.{}){};
+	defer _ = gpa.deinit();
+	const allocator = gpa.allocator();
+
+	_ = linked_list(i32, allocator);
+	
+	
+
+}
